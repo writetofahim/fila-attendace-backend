7aa0eea6a67aa33c3ce8fac07d31ed772247e0f7
Fahim <writetofahimahammad@gmail.com>
2023-02-22 11:19:35 +0600

    updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const jwt = require("jsonwebtoken");
 const db = require("./db");
 const express = require("express");
 const cors = require("cors");
+const { JsonWebTokenError } = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
@@ -23,12 +25,30 @@ app.get("/attendance", (req, res) => {
 });
 app.post("/attendance", (req, res) => {
   const data = req.body;
+  const ip = req.headers["x-forwarded-for"];
+  console.log(`Client IP address: ${ip}`);
   console.log(data);
 });
 app.post("/login", (req, res) => {
   console.log(req);
-  const data = req.body;
-  console.log(data);
+  const { userName, password } = req.body;
+  const query = `SELECT * FROM user WHERE userName = '${userName}' AND password = '${password}'`;
+  db.query(query, [], function (error, results, fields) {
+    if (error) throw error;
+    console.log(results);
+    if (results.length < 1) {
+      return res.status(404).send({ error: "No user found!" });
+    }
+    const user = {
+      userName: results[0].userName,
+      fullName: results[0].fullName,
+      id: results[0].id,
+    };
+    const token = jwt.sign({ userName: user.userName }, "filasco", {
+      expiresIn: "1d",
+    });
+    return res.send({ user, token });
+  });
 });
 app.listen(port, () => {
   console.log("Backend is running on ", port);
